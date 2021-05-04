240dd06644edf6aa5feacf7417bdab87b0a2b1bd
Pedro Ramalho <pedro_hrr@hotmail.com>
2021-05-04 00:10:22 -0300

    Add project files.

---
+++ Heranca/Entities/SavingAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca.Entities
+{
+    class SavingAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+
+        public SavingAccount()
+        { }
+
+        public SavingAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+
+    }
+}
